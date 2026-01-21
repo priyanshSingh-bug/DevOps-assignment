c600d3790f085c04e27af5c067a31ddff41cb0b8
unknown <priyansh.singh@antino.com>
2026-01-21 11:33:34 +0530

    modified shell script file

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,3 @@
-
 set -e
 
 echo "Checking Docker..."
@@ -9,24 +8,18 @@ docker compose version >/dev/null 2>&1 || { echo "Docker Compose not installed";
 
 echo "Stopping old containers..."
 docker compose down -v
+docker compose down # for persisting data as per assignment
 
 echo "Building & starting containers..."
 docker compose up --build -d
 
-# echo "Waiting for app..."
-# sleep 10
-
 echo "Waiting for app to be healthy..."
-
-APP_CONTAINER="devops-assignment-app"
+APP_CONTAINER=$(docker compose ps -q app)
 
 until [ "$(docker inspect -f '{{.State.Health.Status}}' $APP_CONTAINER)" = "healthy" ];
 do
   echo "App not healthy yet... waiting"
   sleep 2
 done
-
-echo "[SUCCESS] Application is live at http://localhost"
-
 
 echo "[SUCCESS] Application is live at http://localhost"
